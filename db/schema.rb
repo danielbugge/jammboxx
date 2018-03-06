194569aa779ee5e8a62bf66ff452e37f007b9778
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-03-06 22:10:08 +0000

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-c# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180306182441) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "time"
     t.index ["genre_id"], name: "index_jamms_on_genre_id"
   end
 
