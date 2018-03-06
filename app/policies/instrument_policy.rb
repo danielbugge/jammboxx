405fc6e0075a4bce02ec11be63f1824d519666bf
Paolo Kuzminsky <paolok89@gmail.com>
2018-03-06 16:27:21 +0000

    routes and policy for instruments

---
+++ app/policies/instrument_policy.rb
@@ -0,0 +1,28 @@
+class InstrumentPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :jamm, only: [ :index, :show, :new, :create  ]
+
+  resources :instruments, only: [ :show, :new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
