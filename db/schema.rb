de838b048351b2839bdd17bd9f943784e16f20e8
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-03-06 18:25:47 +0000

    added lng and lat to jamms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306144555) do
+ActiveRecord::Schema.define(version: 20180306182441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20180306144555) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["genre_id"], name: "index_jamms_on_genre_id"
   end
 
