65cc887329480205c0bb5c9a4783553810f9440b
Daniel Lopez <dlopez.10g@gmail.com>
2018-03-06 23:08:03 +0000

    seed fixes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-c# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@ c# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306182441) do
+ActiveRecord::Schema.define(version: 20180306220627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180306182441) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "time"
     t.index ["genre_id"], name: "index_jamms_on_genre_id"
   end
 
