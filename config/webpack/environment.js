312bc19dbf988b430b8daa853629a261a3df3ea0
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-03-07 19:33:35 +0000

    banner home page

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,7 @@ const { environment } = require('@rails/webpacker')
 
 // Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+environment.plugins.set('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
