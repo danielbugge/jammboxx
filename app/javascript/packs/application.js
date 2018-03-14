dbeba0a24a370a03cf9bdeecd592bb3585896084
jeremy dhanani <Jeremy.dhanani@gmail.com>
2018-03-14 12:45:22 +0000

    slideshow banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ import "bootstrap";
 import { deletePlaceholder }  from '../components/delete_placeholders';
 import { putPlaceholder }  from '../components/put_placeholder';
 import { addSearchResults }  from '../components/search_results';
+import { slideBanner }  from '../components/slider';
 
 deletePlaceholder();
 putPlaceholder();
 addSearchResults();
+slideBanner();
